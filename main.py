90c4dddb52c1d4b76fcca9ca7464bfbce3ba7964
Strangeh21 <93949849+strangeh21@users.noreply.github.com>
2022-07-18 12:05:06 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+import sys
+import os
+
+
+def main():
+    file = sys.argv[1]
+    return_filelist_randomized(file)
+
+
+def return_filelist_randomized(file):
+    num = 0
+    with open(file, "r") as f:
+        results = [line.strip() for line in f]
+    random.shuffle(results)
+    filename, ext = os.path.splitext(file)
+    filename_shuffled = filename + "-SHUFFLED"
+    filename_shuffled2 = filename + "-SHUFFLED"
+    while os.path.exists(f"{filename_shuffled}{ext}"):
+        num += 1
+        filename_shuffled = f"{filename_shuffled2}" + str(num)
+    with open(f"{filename_shuffled}{ext}", "x") as f:
+        for i in results:
+            f.write(i + "\n")
+    return "done"
+
+
+if __name__ == "__main__":
+    main()
